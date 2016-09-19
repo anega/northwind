36f7fc403d73900ddc1e990c8d0c43abc40ca383
anega <anegamega@gmail.com>
2016-09-19 14:25:49 +0300

    Categories server functionality

---
+++ app/routes/categories.server.routes.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function (app) {
+    var categories = require('../../app/controllers/categories.server.controller');
+
+    app.route('/categories')
+        .get(categories.list)
+        .post(categories.create);
+
+    app.route('/categories/:categoryId')
+        .get(categories.read)
+        .put(categories.update)
+        .delete(categories.delete);
+
+    app.param('categoryId', categories.categoryById);
+};
